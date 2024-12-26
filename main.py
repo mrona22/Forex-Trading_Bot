c84a8adfcc1f141df37fafe85638af35719c74fd
milanrona <mrona@ucsd.edu>
2024-12-26 21:01:18 +0100

    updated main and strategry, created feature_engeneering function (still might change tho)

--- main.py
+++ main.py
@@ -2,11 +2,54 @@ import threading
 import pandas as pd
 import numpy as np
 import time
+from data_fetcher import make_request_series, create_info_series, json_to_pandas
+from strategy import run_diagnostics
+from websocket_client import WebSocketClient
+from dotenv import load_dotenv
 
-
-def main():
+def start_socket(client):
     """
-    Main function to run code for the bot
-
+    Function to start the websocket client.
     """
-    pass
+    client.connect()
+
+
+
+def main(ticker_symbol):
+    """
+    Main function to run code for the bot. 
+
+    TODO: Get initial data and implement and optimize machine learning model
+    TODO: Initialize and start the websocket client
+    TODO: Using the model and the new data make pattern recognition and make decisions
+    """
+
+    client = WebSocketClient(ticker_symbol)
+
+    initial_data = json_to_pandas(make_request_series(create_info_series(ticker_symbol, '5m')))
+
+    websocket_thread = threading.Thread(target=start_socket, args=(client,), daemon=True)
+
+    websocket_thread.start()
+
+    while True:
+        available_data  = client.data.get()
+
+        if available_data:
+
+            new_data = available_data['price']
+
+            # Create a decision function which makes a decision based on the new data
+
+            # decision = decision_function(new_data)
+
+            # if decision:
+
+            
+            
+
+
+
+    
+    
+
--- strategy.py
+++ strategy.py
@@ -1,6 +1,46 @@
 import pandas as pd
 import numpy as np
 import logging
+import sklearn as sk
+
+# add logging
+logging.basicConfig(level=logging.INFO)
+
+
+def feature_engineering(dataframe):
+    """
+    Takes in dataframe, with close, high, low, (volume maybe) and returns a dataframe with features:
+    - Rolling Mean
+    - Stochastic Oscillator %K
+    - Williams %R
+    - Weighted Close
+    - Commodity Channel Index (CCI)
+    - 15 Day Moving Average 
+    - 15 Day Rolling Standard Deviation
+    - Bollinger Bands
+    """
+
+    logging.info('Feature Engineering')
+
+    dataframe['rolling_mean'] = dataframe['close'].expanding().mean()
+
+    dataframe['%K'] = ((dataframe['close'] - dataframe['low'].rolling(window=10).min()) / (dataframe['high'].rolling(window=10).max() - dataframe['low'].rolling(window=10).min())) * 100
+
+    dataframe['%R'] = ((dataframe['high'].rolling(window=10).max() - dataframe['close']) / (dataframe['high'].rolling(window=10).max() - dataframe['low'].rolling(window=10).min())) * (-100)
+
+    dataframe['weighted_close'] = (dataframe['high'] + dataframe['low'] + 2 * dataframe['close']) / 4 
+
+    dataframe['CCI'] = (dataframe['weighted_close'] - dataframe['weighted_close'].rolling(window=10).mean()) / (0.015 * dataframe['weighted_close'].rolling(window=10).std())
+
+    dataframe['15_moving_mean'] = dataframe['close'].rolling(window=15).mean()
+
+    dataframe['15_rolling_std'] = dataframe['close'].rolling(window=15).std()
+
+    dataframe['upper_band'] = dataframe['15_moving_mean'] + (2 * dataframe['15_rolling_std'])
+
+    dataframe['lower_band'] = dataframe['15_moving_mean'] - (2 * dataframe['15_rolling_std'])
+
+    return dataframe
 
 def run_diagnostics(dataframe):
     """
